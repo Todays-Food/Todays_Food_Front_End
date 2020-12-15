ef9e99f080b87d6eb487afdaeb82b5218222df02
vreez <dearsoou@gmail.com>
2020-12-15 22:38:28 +0900

    vue 기본 틀 작성

---
+++ todays-food/src/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '@/views/Home'
+import Signup from '@/views/accounts/Signup'
+import Login from '@/views/accounts/Login'
+
+import PostList from '@/views/community/PostList'
+import CreatePost from '@/views/community/CreatePost'
+import Detail from '@/views/community/Detail'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/PostList',
+    name: 'PostList',
+    component: PostList,
+  },
+  {
+    path: '/community/create/:id?',
+    name: 'CreatePost',
+    component: CreatePost,
+  },
+  {
+    path: '/community/:id',
+    name: 'Detail',
+    component: Detail
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
