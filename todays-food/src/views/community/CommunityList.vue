913f705cdfc63e12b6c553f034e515c9d7c6bce4
JEON Si Eun <najse77@naver.com>
2020-12-16 23:34:39 +0900

    community vue 진행중

---
+++ todays-food/src/views/community/CommunityList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+const SERVER_URL = process.env.VUE_APP_SERVER_URL
+
+export default {
+  name: ''
+}
+</script>
+
+<style>
+
+</style>
